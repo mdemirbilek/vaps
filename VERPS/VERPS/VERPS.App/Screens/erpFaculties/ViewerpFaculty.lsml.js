f519340c244561e99f1198d3ddaf319eb6e14b3a
mdemirbilek <mdemirbilek@gmail.com>
2016-11-30 11:13:02 +0100

    ins.

---
+++ VERPS/VERPS/VERPS.App/Screens/erpFaculties/ViewerpFaculty.lsml.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="~/GeneratedArtifacts/viewModel.js" />
+
+myapp.ViewerpFaculty.Details_postRender = function (element, contentItem) {
+    // Write code here.
+    var name = contentItem.screen.erpFaculty.details.getModel()[':@SummaryProperty'].property.name;
+    contentItem.dataBind("screen.erpFaculty." + name, function (value) {
+        contentItem.screen.details.displayName = value;
+    });
+}
+

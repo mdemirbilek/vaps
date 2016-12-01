964ab49b09aedd30ee48a466e3ef465352705527
mdemirbilek <mdemirbilek@gmail.com>
2016-12-01 17:32:07 +0100

    initial

---
+++ DoorDataProcessor/DoorDataProcessor/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess; // namespace that Csv reader lives in
+using System.Data;
+
+
+namespace DoorDataProcessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string op = "";
+
+            try
+            {
+                op = args[0].Trim().ToString();
+            }
+            catch (Exception ex)
+            {
+                op = "";
+            }
+
+            if (op != "pald")
+            {
+                ParseAndLoadData();
+            }
+
+
+
+        }
+
+        private static void ParseAndLoadData()
+        {
+            DateTime dtNow = DateTime.Now;
+            //\\10.10.5.6\DoorExportFiles
+
+            string fileName = @"\\10.10.5.6\DoorExportFiles\DoorsNightlyRawData";
+            fileName = fileName + dtNow.Year.ToString() + get2Digit(dtNow.Month.ToString()) + get2Digit(dtNow.Day.ToString());
+
+            DataAccess.DataTable dt = DataAccess.DataTable.New.ReadCsv(fileName);
+
+            Console.WriteLine(fileName);
+
+            Console.ReadLine();
+
+        }
+
+
+        private static string get2Digit(string s)
+        {
+            string str4Return = "";
+            if (s.Length == 1)
+            {
+                str4Return = "0" + s;
+            }
+            else
+            {
+                str4Return = s;
+            }
+
+            return str4Return;
+        }
+
+        private static System.Data.DataTable GetTable()
+        {
+            // Here we create a DataTable with four columns.
+            System.Data.DataTable table = new System.Data.DataTable();
+            table.Columns.Add("Eventdatetime", typeof(DateTime));
+            table.Columns.Add("EventdatetimeUTC", typeof(DateTime));
+            table.Columns.Add("OperationID", typeof(Int16));
+            table.Columns.Add("Isexit", typeof(Int16));
+            table.Columns.Add("Operationdescription", typeof(string));
+            table.Columns.Add("Usertype", typeof(Int16));
+            table.Columns.Add("Username", typeof(string));
+            table.Columns.Add("UserExtID", typeof(string));
+            table.Columns.Add("UserGPF1", typeof(string));
+            table.Columns.Add("UserGPF2", typeof(string));
+            table.Columns.Add("UserGPF3", typeof(string));
+            table.Columns.Add("UserGPF4", typeof(string));
+            table.Columns.Add("UserGPF5", typeof(string));
+            table.Columns.Add("Cardserialnumber", typeof(string));
+            table.Columns.Add("CardID", typeof(string));
+            table.Columns.Add("Doorname", typeof(string));
+            table.Columns.Add("DoorExtID", typeof(string));
+            table.Columns.Add("DoorGPF1", typeof(string));
+            table.Columns.Add("DoorGPF2", typeof(string));
+
+            return table;
+        }
+
+
+
+    }
+}
--- DoorDataProcessor/DoorDataProcessor/Properties/AssemblyInfo.cs
+++ DoorDataProcessor/DoorDataProcessor/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VERPS.Server")]
+[assembly: AssemblyTitle("DoorDataProcessor")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("VERPS.Server")]
+[assembly: AssemblyProduct("DoorDataProcessor")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ee95ae40-ce85-4fac-9a44-3dd6c633b44a")]
+[assembly: Guid("8907f502-87cf-4e7d-b7e2-663100f03f16")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.49.0")]
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]

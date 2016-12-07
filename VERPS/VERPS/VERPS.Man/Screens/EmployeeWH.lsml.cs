c3a2c3862744fc3dc5f7710a3ebbbf56cb6884dc
mdemirbilek <mdemirbilek@gmail.com>
2016-12-07 17:56:22 +0100

    saas107

---
+++ VERPS/VERPS/VERPS.Man/Screens/EmployeeWH.lsml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.LightSwitch.Presentation.Extensions;
+using Microsoft.LightSwitch.Presentation;
+using Microsoft.LightSwitch.Framework.Client;
+using Microsoft.LightSwitch;
+using System.Collections.Generic;
+using System.IO.IsolatedStorage;
+using System.IO;
+using System.Linq;
+using System;
+
+namespace LightSwitchApplication
+{
+    public partial class EmployeeWH
+    {
+        partial void EmployeeWH_InitializeDataWorkspace(List<IDataService> saveChangesTo)
+        {
+            //this.hrPersonLSUser = this.Application.User.Name;
+            this.RecYear = DateTime.Now.Year;
+            this.RecMonth = DateTime.Now.Month;
+        }
+    }
+}

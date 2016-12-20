d885ff4c76ec139d8f08308eb78cba611c32fc1b
mdemirbilek <mdemirbilek@gmail.com>
2016-12-20 10:08:30 +0100

    sasasasa

--- VERPS/VERPS/VERPS.Man/UserCode/Application.cs
+++ VERPS/VERPS/VERPS.Man/UserCode/Application.cs
@@ -18,16 +18,7 @@ namespace LightSwitchApplication
 
         }
 
-        partial void Faculties_CanRun(ref bool result)
-        {
-            result = Application.Current.User.HasPermission(Permissions.SecurityAdministration);
-        }
 
-        partial void Universities_CanRun(ref bool result)
-        {
-            result = Application.Current.User.HasPermission(Permissions.SecurityAdministration);
-
-        }
 
         partial void Departments_CanRun(ref bool result)
         {
@@ -39,25 +30,7 @@ namespace LightSwitchApplication
             result = Application.Current.User.HasPermission(Permissions.SecurityAdministration);
         }
 
-        partial void NewPerson_CanRun(ref bool result)
-        {
-            result = Application.Current.User.HasPermission(Permissions.SecurityAdministration);
-        }
 
-        partial void WorkingHours_CanRun(ref bool result, string LSUser)
-        {
-            result = Application.Current.User.HasPermission(Permissions.SecurityAdministration);
-        }
-
-        partial void EmpWorkingHours_CanRun(ref bool result)
-        {
-            result = Application.Current.User.HasPermission(Permissions.SecurityAdministration);
-        }
-
-        partial void WHAssignment_CanRun(ref bool result)
-        {
-            result = Application.Current.User.HasPermission(Permissions.SecurityAdministration);
-        }
 
         partial void WHSupervision_CanRun(ref bool result)
         {
@@ -69,64 +42,8 @@ namespace LightSwitchApplication
             result = Application.Current.User.HasPermission(Permissions.HRManagement);
         }
 
-        partial void AcademicDepartments_CanRun(ref bool result)
-        {
-            result = Application.Current.User.HasPermission(Permissions.SecurityAdministration);
-        }
 
-        partial void AcademicalAims_CanRun(ref bool result)
-        {
-            result = Application.Current.User.HasPermission(Permissions.SecurityAdministration);
-        }
 
-        partial void CourseMaterials_CanRun(ref bool result)
-        {
-            result = Application.Current.User.HasPermission(Permissions.SecurityAdministration);
-        }
 
-        partial void CourseTeachers_CanRun(ref bool result)
-        {
-            result = Application.Current.User.HasPermission(Permissions.SecurityAdministration);
-        }
-
-        partial void Courses_CanRun(ref bool result)
-        {
-            result = Application.Current.User.HasPermission(Permissions.SecurityAdministration);
-        }
-
-        partial void LearningOutcomes_CanRun(ref bool result)
-        {
-            result = Application.Current.User.HasPermission(Permissions.SecurityAdministration);
-        }
-
-        partial void Prerequisites_CanRun(ref bool result)
-        {
-            result = Application.Current.User.HasPermission(Permissions.SecurityAdministration);
-        }
-
-        partial void ProgramCourses_CanRun(ref bool result)
-        {
-            result = Application.Current.User.HasPermission(Permissions.SecurityAdministration);
-        }
-
-        partial void Syllabi_CanRun(ref bool result)
-        {
-            result = Application.Current.User.HasPermission(Permissions.SecurityAdministration);
-        }
-
-        partial void SyllabusNotes_CanRun(ref bool result)
-        {
-            result = Application.Current.User.HasPermission(Permissions.SecurityAdministration);
-        }
-
-        partial void SyllabusTopics_CanRun(ref bool result)
-        {
-            result = Application.Current.User.HasPermission(Permissions.SecurityAdministration);
-        }
-
-        partial void WorkloadStructures_CanRun(ref bool result)
-        {
-            result = Application.Current.User.HasPermission(Permissions.SecurityAdministration);
-        }
     }
 }
